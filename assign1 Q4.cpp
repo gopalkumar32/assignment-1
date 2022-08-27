237a66f2f5c5b8e3009f80b2b1d377883edfd8df
gopalkumar32 <109418744+gopalkumar32@users.noreply.github.com>
2022-08-27 22:25:02 +0530

    Add files via upload

---
+++ assign1 Q4.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float r ,area;
+	printf("Entre the value of radius");
+	scanf("%f",&r);
+	area=3.14*r*r;
+	printf("Area of circle is %2f having the radius %f",area,r);
+}
---
+++ assign1 Q5.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#include<string.h>
+ main()
+{
+	char s[20];
+	int i;
+	printf("Entre the string");
+	get(s);
+	i=strlen(s);
+	printf("Length is %d",i);
+	
+}
+
